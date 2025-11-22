e95e6b0b1874773a07fd73271940b1e8e980acbd
Daniel257p <daniel257.paulinopolanco@gmail.com>
2025-11-22 11:58:36 -0600

    fix images errors

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -14,56 +14,49 @@ const temples = [
     location: "Aba, Nigeria",
     dedicated: "2005, August, 7",
     area: 11500,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    imageUrl: "images/aba-nigeria-temple.webp" 
   },
   {
     templeName: "Manti Utah",
     location: "Manti, Utah, United States",
     dedicated: "1888, May, 21",
     area: 74792,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    imageUrl: "images/manti-utah-temple.webp"
   },
   {
     templeName: "Payson Utah",
     location: "Payson, Utah, United States",
     dedicated: "2015, June, 7",
     area: 96630,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    imageUrl: "images/payson-utah-temple.webp"
   },
   {
     templeName: "Yigo Guam",
     location: "Yigo, Guam",
     dedicated: "2020, May, 2",
     area: 6861,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    imageUrl: "images/yigo-guam-temple.webp"
   },
   {
     templeName: "Washington D.C.",
     location: "Kensington, Maryland, United States",
     dedicated: "1974, November, 19",
     area: 156558,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    imageUrl: "images/washington-temple.webp"
   },
   {
     templeName: "Lima Perú",
     location: "Lima, Perú",
     dedicated: "1986, January, 10",
     area: 9600,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    imageUrl: "images/lima-peru-temple.webp"
   },
   {
     templeName: "Mexico City Mexico",
     location: "Mexico City, Mexico",
     dedicated: "1983, December, 2",
     area: 116642,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    imageUrl: "images/mexico-city-mexico-temple.webp"
   },
   // Add more temple objects here...
   {
@@ -71,21 +64,21 @@ const temples = [
     location: "Santo Domingo Dominican Republic",
     dedicated:" 17 September 2000" ,
     area: 67000,
-    imageUrl:"https://churchofjesuschristtemples.org/assets/img/temples/santo-domingo-dominican-republic-temple/santo-domingo-dominican-republic-temple-1444-main.jpg"
+    imageUrl:"images/santo-domingo-dominican-republic-temple.webp"
   },
   {
     templeName: "Barranquilla Colombia" ,
     location: "Barranquilla Colombia" ,
     dedicated:" 9 December 2018" ,
     area: 25349,
-    imageUrl:"https://churchofjesuschristtemples.org/assets/img/temples/barranquilla-colombia-temple/barranquilla-colombia-temple-1846-main.jpg"
+    imageUrl:"images/barranquilla-colombia-temple.webp"
     },
     {
     templeName: "Sydney Australia" ,
     location: "Sydney Australia",
     dedicated:" 24 November 1991 " ,
     area:30067,
-    imageUrl:"https://churchofjesuschristtemples.org/assets/img/temples/sydney-australia-temple/sydney-australia-temple-43342-main.jpg"
+    imageUrl:"images/sydney-australia-temple.webp"
     }
 ];
 
