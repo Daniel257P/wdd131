e98337a48668422ccc217f0208f6ef5584dfee8a
Daniel257p <daniel257.paulinopolanco@gmail.com>
2025-11-29 13:53:30 -0600

    week5 assignment

---
+++ scripts/form.js
@@ -0,0 +1,26 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const products = [
+  { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
+  { id: "fc-2050", name: "power laces", averagerating: 4.7 },
+  { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
+  { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
+  { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+];
+
+const selectElement = document.getElementById("productName");
+
+products.forEach(product => {
+  const option = document.createElement("option");
+  option.value = product.id;       
+  option.textContent = product.name; 
+  selectElement.appendChild(option);
+});
+
+  let reviewCount = localStorage.getItem("reviewCount");
+  if (!reviewCount) {
+    reviewCount = 0;
+  }
+  reviewCount = parseInt(reviewCount) + 1;
+  localStorage.setItem("reviewCount", reviewCount);
